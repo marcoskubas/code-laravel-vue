0a20c6621dcd603cedb1c7fc9d66367e9257e424
Marcos Kubas <marcosarobed@gmail.com>
2018-05-16 11:05:50 -0300

    CONFIGURANDO WEBPACK - vue-hot-reload-api

--- resources/assets/admin/js/components/Example.vue
+++ resources/assets/admin/js/components/Example.vue
@@ -3,10 +3,10 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Example Component</div>
+                    <div class="panel-heading">Example Component {{ tempo }}</div>
 
                     <div class="panel-body">
-                        I'm an example component!
+                        I'm an example component! Marcos Kubas {{teste}}
                     </div>
                 </div>
             </div>
@@ -16,8 +16,17 @@
 
 <script>
     export default {
-        mounted() {
-            console.log('Component mounted.')
+        data(){
+            return {
+                tempo : Date.now(),
+                teste : 'Vue.js'
+            }
+        },
+        created(){
+            console.log('created');
+        },
+        ready(){
+            console.log('ready');
         }
     }
 </script>
