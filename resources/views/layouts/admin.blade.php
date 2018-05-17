98ad8ede22e5b5414022f32a422f83aaad4b9973
Marcos Kubas <marcosarobed@gmail.com>
2018-05-17 13:55:26 -0300

    PAINEL ADMINISTRATIVO - Configurando áreas da aplicação

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -22,32 +22,42 @@
 </head>
 <body>
     <div id="app">
-    
-        @if (Auth::check())
-        <?php 
-        $menuConfig = [
-            'name'  => Auth::user()->name,
-            'menus' => [
-                ['name' => 'Contas a pagar', 'url' => '/teste', 'dropdownId' => 'teste'],
-                ['name' => 'Contas a receber', 'url' => '/teste1']
-            ],
-            'menusDropdown' => [
-                [
-                    'id' => 'teste', 
-                    'items' => [
-                        ['name' => 'Listar Contas', 'url' => '/lista'],
-                        ['name' => 'Criar Conta', 'url' => '/criar']
+        <header>
+            @if (Auth::check())
+            <?php 
+            $menuConfig = [
+                'name'  => Auth::user()->name,
+                'menus' => [
+                    ['name' => 'Contas a pagar', 'url' => '/teste', 'dropdownId' => 'teste'],
+                    ['name' => 'Contas a receber', 'url' => '/teste1']
+                ],
+                'menusDropdown' => [
+                    [
+                        'id' => 'teste', 
+                        'items' => [
+                            ['name' => 'Listar Contas', 'url' => '/lista'],
+                            ['name' => 'Criar Conta', 'url' => '/criar']
+                        ]
                     ]
-                ]
-            ],
-            'urlLogout' => '/admin/logout',
-            'csrfToken' => csrf_token()
-        ];
-        ?>
-        <admin-menu :config="{{ json_encode($menuConfig) }}"></admin-menu>
-        @endif
+                ],
+                'urlLogout' => '/admin/logout',
+                'csrfToken' => csrf_token()
+            ];
+            ?>
+            <admin-menu :config="{{ json_encode($menuConfig) }}"></admin-menu>
+            @endif
+        </header>
 
-        @yield('content')
+        <main>@yield('content')</main>
+
+        <footer class="page-footer">
+            <div class="footer-copyfight">
+                <div class="container">
+                    {{date('Y')}} <a class="grey-text text-lighten-4" href="http://localhost:3001/admin/home">Laravel Vue Admin</a>
+                </div>
+            </div>
+        </footer>
+
     </div>
 
     <!-- Scripts -->
