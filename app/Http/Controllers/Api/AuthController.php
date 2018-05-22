80dc0d0eff1b3c392fd04652136d11aac554df3a
Marcos Kubas <marcosarobed@gmail.com>
2018-05-22 08:53:48 -0300

    AUTENTICAÇÃO COM JWT - Tratamento de erros de login

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -6,25 +6,81 @@ use CodeLaravelVue\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Lang;
 
 class AuthController extends Controller
 {
     use AuthenticatesUsers;
 
     public function accessToken(Request $request){
+        
         $this->validateLogin($request);
+
+        // If the class is using the ThrottlesLogins trait, we can automatically throttle
+        // the login attempts for this application. We'll key this by the username and
+        // the IP address of the client making these requests into this application.
+        if ($this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+
+            return $this->sendLockoutResponse($request);
+        }
 
         $credentials = $this->credentials($request);
 
         if($token = Auth::guard('api')->attempt($credentials)){
             return $this->sendLoginResponse($request, $token);
         }
+
+        $this->incrementLoginAttempts($request);
+
+        return $this->sendFailedLoginResponse($request);
     }
 
     protected function sendLoginResponse(Request $request, $token){
+
+        $this->clearLoginAttempts($request);
+
         return response()->json([
             'token' => $token
         ]);
+    }
+
+     /**
+     * Clear the login locks for the given user credentials.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return void
+     */
+    protected function clearLoginAttempts(Request $request)
+    {
+        $this->limiter()->clear($this->throttleKey($request));
+    }
+
+    public function sendLockoutResponse(Request $request){
+        $seconds = $this->limiter()->availableIn(
+            $this->throttleKey($request)
+        );
+
+        $message = Lang::get('auth.throttle', ['seconds' => $seconds]);
+
+        return response()->json([
+            'message' => $message,
+        ], 403);
+    }
+
+    /**
+     * Get the failed login response instance.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    protected function sendFailedLoginResponse(Request $request)
+    {
+        $message = Lang::get('auth.failed');
+
+        return response()->json([
+            'message' => $message
+        ], 401);
     }
 
     /**
