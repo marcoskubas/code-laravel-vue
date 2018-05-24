a671e698853ec2a6e1653ebe043ba463052a4be6
Marcos Kubas <marcosarobed@gmail.com>
2018-05-24 09:59:53 -0300

    INICIANDO SPA - Local Storage

--- resources/assets/spa/js/spa.js
+++ resources/assets/spa/js/spa.js
@@ -4,3 +4,8 @@ require('vue-resource');
 Vue.http.options.root = "http://localhost:8000/api";
 
 require('./router');
+
+window.localStorage.setItem('name', 'value');
+window.localStorage.setItem('name', 'alterValue');
+window.localStorage.clear();
+window.localStorage.removeItem('name');
