f247aaedc586a9d70a82407297d36df0a3558029
Marcos Kubas <marcosarobed@gmail.com>
2018-06-04 11:30:19 -0300

    CRUD DE BANCOS - Exclusão modal

--- resources/views/admin/banks/index.blade.php
+++ resources/views/admin/banks/index.blade.php
@@ -21,9 +21,23 @@
                 <td>{{ $bank->id }}</td>
                 <td>{{ $bank->name }}</td>
                 <td>
+                    <?php $modalId =  "modal-delete-{$bank->ID}"; ?> 
                     <a href="{{route('admin.banks.edit', ['bank' => $bank->id])}}">Editar</a> |
                     <delete-action action="{{route('admin.banks.destroy', ['bank' => $bank->id])}}" action-element="link-delete-{{$bank->id}}" csrf-token="{{csrf_token()}}">
-                        <a id="link-delete-{{$bank->id}}" href="{{route('admin.banks.destroy', ['bank' => $bank->id])}}">Excluir</a>
+                        <a id="link-modal-{{$bank->id}}" href="#{{$modalId}}">Excluir</a>
+                        <modal :modal="{{ json_encode(['id' => $modalId])}}" style="display: none">
+                            <div slot="content">
+                                <h5>Mensagem de confirmação</h5>
+                                <p><strong>Deseja excluir este banco?</strong></p>
+                                <div class="divider"></div>
+                                <p>Nome: <strong>{{$bank->name}}</strong></p>
+                                <div class="divider"></div>
+                            </div>
+                            <div slot="footer">
+                                <button id="link-delete-{{$bank->id}}" class="btn btn-flat waves-effect green lighten-2 modal-close modal-action">OK</button>
+                                <button class="btn btn-flat waves-effect waves-red lighten-2 modal-close modal-action">Cancelar</button>
+                            </div>
+                        </modal>
                     </delete-action>
                 </td>
             </tr>
