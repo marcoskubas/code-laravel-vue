ac5d857ba2dde8210d921dbae09b44ec606615c7
Marcos Kubas <marcosarobed@gmail.com>
2018-06-08 09:02:28 -0300

    CRUD DE BANCOS - Mostrando logo do banco

--- resources/views/admin/banks/index.blade.php
+++ resources/views/admin/banks/index.blade.php
@@ -9,36 +9,51 @@
         <table class="bordered striped highlight centered responsive-table z-depth-5">
             <thead>
             <tr>
-                <td>#</td>
-                <td>Nome</td>
-                <td>Ações</td>
+                <th>#</th>
+                <th>Nome</th>
+                <th>Ações</th>
             </tr>
             </thead>
             <tbody>
 
             @foreach($banks as $bank)
             <tr>
-                <td>{{ $bank->id }}</td>
-                <td>{{ $bank->name }}</td>
                 <td>
-                    <?php $modalId =  "modal-delete-{$bank->ID}"; ?> 
-                    <a href="{{route('admin.banks.edit', ['bank' => $bank->id])}}">Editar</a> |
-                    <delete-action action="{{route('admin.banks.destroy', ['bank' => $bank->id])}}" action-element="link-delete-{{$bank->id}}" csrf-token="{{csrf_token()}}">
-                        <a id="link-modal-{{$bank->id}}" href="#{{$modalId}}">Excluir</a>
-                        <modal :modal="{{ json_encode(['id' => $modalId])}}" style="display: none">
-                            <div slot="content">
-                                <h5>Mensagem de confirmação</h5>
-                                <p><strong>Deseja excluir este banco?</strong></p>
-                                <div class="divider"></div>
-                                <p>Nome: <strong>{{$bank->name}}</strong></p>
-                                <div class="divider"></div>
-                            </div>
-                            <div slot="footer">
-                                <button id="link-delete-{{$bank->id}}" class="btn btn-flat waves-effect green lighten-2 modal-close modal-action">OK</button>
-                                <button class="btn btn-flat waves-effect waves-red lighten-2 modal-close modal-action">Cancelar</button>
-                            </div>
-                        </modal>
-                    </delete-action>
+                    <div class="row valign-wrapper">
+                        <div class="col s12">{{ $bank->id }}</div>
+                    </div>
+                </td>
+                <td>
+                    <div class="row valign-wrapper">
+                        <div class="col s2"><img src="{{ asset("storage/banks/images/{$bank->logo}") }}" alt="{{ $bank->name }}" class="bank-logo" /></div>
+                        <div class="col s10">
+                            <span class="left">{{ $bank->name }}</span>
+                        </div>
+                    </div>
+                </td>
+                <td>
+                    <div class="row valign-wrapper">
+                        <div class="col s12">
+                            <?php $modalId =  "modal-delete-{$bank->ID}"; ?> 
+                            <a href="{{route('admin.banks.edit', ['bank' => $bank->id])}}">Editar</a> |
+                            <delete-action action="{{route('admin.banks.destroy', ['bank' => $bank->id])}}" action-element="link-delete-{{$bank->id}}" csrf-token="{{csrf_token()}}">
+                                <a id="link-modal-{{$bank->id}}" href="#{{$modalId}}">Excluir</a>
+                                <modal :modal="{{ json_encode(['id' => $modalId])}}" style="display: none">
+                                    <div slot="content">
+                                        <h5>Mensagem de confirmação</h5>
+                                        <p><strong>Deseja excluir este banco?</strong></p>
+                                        <div class="divider"></div>
+                                        <p>Nome: <strong>{{$bank->name}}</strong></p>
+                                        <div class="divider"></div>
+                                    </div>
+                                    <div slot="footer">
+                                        <button id="link-delete-{{$bank->id}}" class="btn btn-flat waves-effect green lighten-2 modal-close modal-action">OK</button>
+                                        <button class="btn btn-flat waves-effect waves-red lighten-2 modal-close modal-action">Cancelar</button>
+                                    </div>
+                                </modal>
+                            </delete-action>  
+                        </div>
+                    </div>
                 </td>
             </tr>
             @endforeach
