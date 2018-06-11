5af6b5d9d776f931558cfb963c25505750e22d35
Marcos Kubas <marcosarobed@gmail.com>
2018-06-11 09:55:24 -0300

    PAGINAÇÃO NO SPA - Listando contas bancárias

--- resources/assets/spa/js/components/Menu.vue
+++ resources/assets/spa/js/components/Menu.vue
@@ -47,18 +47,9 @@
 		data(){
 			return {
 				menus: [
-					{name: 'Contas a pagar', dropdownId: 'teste'},
-					{name: 'Contas a receber', routeName: 'auth.login'}
+					{name: 'Conta bancária', routeName: 'bank-account.list'},
 				],
-				menusDropdown: [
-					{
-						id: 'teste',
-						items: [
-							{name: 'Listar contas', routeName: 'auth.login'},
-							{name: 'Criar conta', routeName: 'auth.login'},
-						]
-					}
-				],
+				menusDropdown: [],
 				user : Auth.user
 			}
 		},
--- resources/assets/spa/js/services/resources.js
+++ resources/assets/spa/js/services/resources.js
@@ -15,6 +15,7 @@ export class Jwt {
 	}
 }
 
-let User = Vue.resource('user');
+let User        = Vue.resource('user');
+let BankAccount = Vue.resource('bank_accounts');
 
-export {User};
+export {User, BankAccount};
