960858285b40cd06c8688e4ea3f22d46c0d68055
Marcos Kubas <marcosarobed@gmail.com>
2018-06-11 14:33:52 -0300

    PAGINAÇÃO NO SPA - Mostrando páginas

--- resources/assets/spa/js/components/Pagination.vue
+++ resources/assets/spa/js/components/Pagination.vue
@@ -1,5 +1,21 @@
 <template>
-	<div>{{pages}}</div>
+	<ul class="pagination">
+		<li :class="{'disabled' : currentPage == 0}">
+			<a @click.prevent="previousPage" href="#">
+				<i class="material-icons">chevron_left</i>
+			</a>
+		</li>
+
+		<li v-for="o in pages" class="waves-effect" :class="{'active' : currentPage == o}">
+			<a @click.prevent="setCurrentPage(o)" href="#">{{ o + 1}}</a>
+		</li>
+
+		<li :class="{'disabled' : currentPage == pages - 1}">
+			<a @click.prevent="nextPage" href="#">
+				<i class="material-icons">chevron_right</i>
+			</a>
+		</li>
+	</ul>
 </template>
 
 <script type="text/javascript">
@@ -22,6 +38,21 @@
 			pages(){
 				let pages = Math.ceil(this.totalRecords / this.perPage);
 				return Math.max(pages, 1);
+			}
+		},
+		methods : {
+			previousPage(){
+				if(this.currentPage > 0){
+					this.currentPage--;
+				}
+			},
+			setCurrentPage(page){
+				this.currentPage = page;
+			},
+			nextPage(){
+				if(this.currentPage < this.pages - 1){
+					this.currentPage++;
+				}
 			}
 		}
 	}
