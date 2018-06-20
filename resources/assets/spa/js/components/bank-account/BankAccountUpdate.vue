0822071b1a0470912901877ddce3fff3d9978223
Marcos Kubas <marcosarobed@gmail.com>
2018-06-20 13:45:19 -0300

    CATEGORIAS - Edição de conta bancária

--- resources/assets/spa/js/components/bank-account/BankAccountUpdate.vue
+++ resources/assets/spa/js/components/bank-account/BankAccountUpdate.vue
@@ -3,6 +3,8 @@
 <script type="text/javascript">
     import {BankAccount, Bank} from "../../services/resources";
     import PageTitleComponent from '../PageTitle.vue';
+    import 'materialize-autocomplete';
+    import _ from 'lodash';
 
     export default {
         components: {
@@ -17,6 +19,9 @@
                     account  : '',
                     bank_id  : '',
                     'default': false
+                },
+                bank: {
+                    name: ""
                 },
                 banks: []
             };
@@ -36,12 +41,45 @@
             getBanks(){
                 Bank.query().then((response) => {
                     this.banks = response.data.data;
+                    this.initAutocomplete();
                 });
             },
             getBankAccount(id){
-                BankAccount.get({id: id}).then((response) => {
+                BankAccount.get({id: id, include: 'bank'}).then((response) => {
                     this.bankAccount = response.data.data;
+                    this.bank        = response.data.data.bank.data;
                 })
+            },
+            initAutocomplete(){
+                let self = this;
+                $(document).ready(() => {
+                    $('#bank-id').materialize_autocomplete({
+                        limit: 10,
+                        multiple: {
+                            enable: false
+                        },
+                        dropdown: {
+                            el: '#bank-id-dropdown'
+                        },
+                        getData(value, callback){
+                            let banks = self.filterBankByName(value);
+                            banks = banks.map((o) => {
+                                return {id: o.id, text: o.name};
+                            });
+                            callback(value, banks);
+                        },
+                        onSelect(item){
+                            console.log(item);
+                            self.bankAccount.bank_id = item.id;
+                        }
+                    });
+                });
+            },
+            filterBankByName(name){
+                let banks = _.filter(this.banks, (o) => {
+                    return _.includes(o.name.toLowerCase(), name.toLowerCase());
+                });
+                return banks;
             }
         }
     }
