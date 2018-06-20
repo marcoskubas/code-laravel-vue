950ec1b9602e0b55c698e057686e576da10cfb20
Marcos Kubas <marcosarobed@gmail.com>
2018-06-20 15:03:06 -0300

    CATEGORIAS - CRUD de categories alinhadas

--- app/Repositories/CategoryRepositoryEloquent.php
+++ app/Repositories/CategoryRepositoryEloquent.php
@@ -16,6 +16,43 @@ use CodeLaravelVue\Presenters\CategoryPresenter;
  */
 class CategoryRepositoryEloquent extends BaseRepository implements CategoryRepository
 {
+    public function create(array $attributes){
+
+        if(isset($attributes['parent_id'])){
+            //child
+            $skipPresenter = $this->skipPresenter;
+            $this->skipPresenter(true);
+            $parent = $this->find($attributes['parent_id']);
+            $this->skipPresenter = $skipPresenter;
+            $child = $parent->children()->create($attributes);
+
+            return $this->parserResult($child);
+
+        }else{
+            //parent
+            return parent::create($attributes);
+        }
+    }
+
+    public function update(array $attributes, $id){
+        
+        if(isset($attributes['parent_id'])){
+            //child
+            $skipPresenter = $this->skipPresenter;
+            $this->skipPresenter(true);
+            $child = $this->find($id);
+            $child->parent_id = $attributes['parent_id'];
+            $child->save();
+            $this->skipPresenter = $skipPresenter;
+
+            return $this->parserResult($child);
+
+        }else{
+            //parent
+            return parent::update($attributes, $id);
+        }
+    }
+
     /**
      * Specify Model class name
      *
